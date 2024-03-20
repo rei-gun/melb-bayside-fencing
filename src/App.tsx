1d6179deeb791ec351519f56ffed945266d92c0e
ReiGun <rei@kickavenue.com>
2024-03-20 14:32:49 +0700

    top half done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import rightBG from './assets/right-bg.png'; // Import the background image
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Split-container">
+        <div className="Left-side">
+          <img src={logo} alt="Top Image" className="Top-image" />
+          <div className="Text-block Text-block-1">MELBOURNE</div>
+          <div className="Text-block Text-block-2">BAYSIDE FENCING</div>
+          <div className="Text-block Text-block-3">Simo & Lewis</div>
+        </div>
+        <div className="Right-side"></div> {/* This will have the background image */}
+      </div>
     </div>
   );
 }
